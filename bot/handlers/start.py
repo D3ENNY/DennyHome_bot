1c67ad16b9eb2699102c0491016a933c59509c21
D3ENNY <denysraimondi06@gmail.com>
2026-01-11 23:34:17 +0100

    conclude start command

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -5,9 +5,9 @@ def register(app):
     @app.on_message(filters.command("start"))
     async def start_handler(_, message):
         await message.reply(
-            "Bot DennyHome attivato!\n"
-            "**Lista comandi:**"
-            "/add_device -> registra nuovo dispositivo"
-            "/status -> verifica lo stato di un dispositivo"
+            "Bot DennyHome attivato!\n\n"
+            "**Lista comandi:**\n"
+            "/add_device -> registra nuovo dispositivo\n"
+            "/status -> verifica lo stato di un dispositivo\n"
             "/wake -> sfritta wol per accendere un dispositivo"
         )
